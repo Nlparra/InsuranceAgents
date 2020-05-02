6c9f3466193e6cde0ed1ef7eb86e6bc6a4f70986
Nelson <nlparra.dev@gmail.com>
2020-05-02 22:57:41 +0000

    work on registration view and fetch

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,19 @@
+import { home } from "./../views/home";
+
+const url = "https://randomuser.me/api/";
+
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			Users: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			getUser() {
+				fetch(url)
+					.then(res => res.json())
+					.then(data => {
+						console.log("Get Users", data), setStore({ Users: data });
+					});
 			}
 		}
 	};
--- src/js/views/Register.js
+++ src/js/views/Register.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Card, Button } from "react-bootstrap";
+import rigo from "./../../img/rigo-baby.jpg";
 
 export const Register = () => {
 	return (
-		<div>
-			<div>Hello ! sorry this page is under contruction - Please check back later!</div>
-		</div>
+		<Card className="mt-5" style={{ width: "18rem" }}>
+			<Card.Img variant="top" src={rigo} />
+			<h1>Hello page is under construction</h1>
+		</Card>
 	);
 };
