6a17a898f0e6d4e2c0a93c9f1f6f032ffc6c0dbf
Nelson <nlparra.dev@gmail.com>
2020-05-06 23:21:22 +0000

    work on register input

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,18 +6,18 @@ export const Navbar = () => {
 	return (
 		<>
 			<nav className="navbar navbar-dark bg-dark fixed-top">
-				{/* <Link to="/"> */}
-				<a className="navbar-brand" href="#">
-					{/* <img
+				<Link to="/">
+					<a className="navbar-brand" href="#">
+						{/* <img
 					src="/docs/4.4/assets/brand/bootstrap-solid.svg"
 					width="30"
 					height="30"
 					className="d-inline-block align-top"
 					alt=""
 				/> */}
-					InsurAgent
-				</a>
-				{/* </Link> */}
+						InsurAgent
+					</a>
+				</Link>
 				<form className="form-inline">
 					<SignIn />
 					<Link to="/Register">
--- src/js/views/Register.js
+++ src/js/views/Register.js
@@ -4,9 +4,70 @@ import rigo from "./../../img/rigo-baby.jpg";
 
 export const Register = () => {
 	return (
-		<Card className="mt-5" style={{ width: "18rem" }}>
-			<Card.Img variant="top" src={rigo} />
-			<h1>Hello page is under construction</h1>
-		</Card>
+		<div className="container mt-5">
+			<div className="row ">
+				<div className="col-6">
+					<img className="mt-5" src="https://placehold.it/300x300" />
+					<div className="form-group mt-3">
+						<label htmlFor="myfile">Select a file:</label>
+						<input type="file" id="myfile" name="myfile" />
+						<input type="submit" value="Submit" />
+					</div>
+				</div>
+				<div className="col-6">
+					<h1 className="text-center mt-5">Add a new contact</h1>
+					<form>
+						<div className="form-group">
+							<label>First Name</label>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="please enter your first name"
+								// onChange={e => setName(e.target.value)}
+							/>
+						</div>
+						<div className="form-group">
+							<label>Last Name</label>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Please Enter your last name"
+								// onChange={e => setName(e.target.value)}
+							/>
+						</div>
+						<div className="form-group">
+							<label>Username</label>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Please pick a username"
+								// onChange={e => setName(e.target.value)}
+							/>
+						</div>
+						<div className="form-group">
+							<label>Email</label>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Please enter your email"
+								// onChange={e => setName(e.target.value)}
+							/>
+						</div>
+						<div className="form-group">
+							<label>Password</label>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Please choose a password"
+								// onChange={e => setName(e.target.value)}
+							/>
+						</div>
+						<button type="button" className="btn btn-primary form-control mb-5">
+							save
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>
 	);
 };
