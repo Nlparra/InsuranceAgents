536be569161873e7e6b5754b0ecda8ac9a9b0c08
Nelson <nlparra.dev@gmail.com>
2020-05-12 00:45:02 +0000

    fix code to pull information to agent page

--- src/js/views/AgentPage.js
+++ src/js/views/AgentPage.js
@@ -3,22 +3,24 @@ import { Context } from "../store/appContext";
 import PropTypes from "prop-types";
 
 export const AgentPage = props => {
+	const { uuid } = props.match.params;
 	const { store, actions } = useContext(Context);
 
-	
+	// console.log("test", store.users.filter(user => user.login.uuid === uuid));
+	let izzy = store.users.filter(user => user.login.uuid === uuid);
+	//console.log("uuid =>", uuid);
 
 	return (
 		<div className="contain mt-5">
 			<div className="row">
 				<div className="col-6">
 					<h1>YOOOOOO</h1>
-					{/* <img src={store.users[props.match.params.uuid].picture.medium} /> */}
+					<img src={izzy[0].picture.medium} />
 				</div>
 				<div className="col-6">steve</div>
 			</div>
 			<div className="row" />
-			defaultValue=
-			{store.users[props.match.params.uuid].name}
+			{/* {store.users[props.match.params.uuid].name} */}
 		</div>
 	);
 };
