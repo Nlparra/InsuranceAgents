c1629c704a369c323556237ff568fb6f66bb9704
Nelson <nlparra.dev@gmail.com>
2020-05-12 21:44:37 +0000

    work on agents page

--- src/js/views/AgentPage.js
+++ src/js/views/AgentPage.js
@@ -7,17 +7,22 @@ export const AgentPage = props => {
 	const { store, actions } = useContext(Context);
 
 	// console.log("test", store.users.filter(user => user.login.uuid === uuid));
-	let izzy = store.users.filter(user => user.login.uuid === uuid);
-	//console.log("uuid =>", uuid);
+	let agent = store.users.filter(agent => agent.login.uuid === uuid);
+	console.log("uuid =>", agent);
 
 	return (
-		<div className="contain mt-5">
+		<div className="agentspage contain">
 			<div className="row">
 				<div className="col-6">
-					<h1>YOOOOOO</h1>
-					<img src={izzy[0].picture.medium} />
+					<h1>{`${agent[0].name.first}  ${agent[0].name.last}`}</h1>
+					<img src={agent[0].picture.medium} />
 				</div>
-				<div className="col-6">steve</div>
+				<div className="col-6">
+					Phone Number:
+					{agent[0].phone} <br />
+					Email Address: {agent[0].email} <br />
+					Location: {agent[0].location.country},{agent[0].location.state},{agent[0].location.city} <br />
+				</div>
 			</div>
 			<div className="row" />
 			{/* {store.users[props.match.params.uuid].name} */}
